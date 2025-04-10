c664a34b77aecdb1b0811dff204d5ebb9d53fff6
Barni <tothbarni.06@gmail.com>
2025-04-10 11:17:54 +0200

    alapok

--- AsideKosar.js
+++ AsideKosar.js
@@ -1,1 +1,11 @@
-sa
+class AsideKosar {
+  constructor(index, adat) {
+    this.index = index;
+    this.adat = adat;
+    this.szuloElem = null;
+  }
+
+  megjelenit() {
+    // Oldalsáv kosár megjelenítési logika
+  }
+}
