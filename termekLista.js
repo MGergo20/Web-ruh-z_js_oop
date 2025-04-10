3dd267f2dbcd5cdd7023185723756cfad3c644bf
Medgyaszay Gergő (Szf_N_2024_2026) <medgyaszay.gergo@diak.szamalk-szalezi.hu>
2025-04-10 11:21:34 +0200

    11.21

---
+++ termekLista.js
@@ -0,0 +1,30 @@
+
+const termekLista = [ {
+    id: 1,
+    nev: 'Apple AirPods 2',
+    ar: 114290, 
+    kep: 'airpods.jpg',
+}, 
+{
+    id: 2,
+    nev: 'Xiaomi Mi Watch, Amazfit',
+    ar: 39990,
+    kep: 'huawei.jpg'
+}, 
+{
+    id: 3,
+    nev: 'Háttérvilágítás TV-hez',
+    ar: 15000,
+    kep: 'led.jpg',
+}, 
+{
+    id: 4,
+    nev: 'Mini dron',
+    ar: 50000,
+    kep: 'dron.jpg'
+}, 
+]
+
+
+
+
