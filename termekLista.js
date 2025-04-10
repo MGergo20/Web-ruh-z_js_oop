ef097db18ac529068101158d5495e58ba53386d1
Barni <tothbarni.06@gmail.com>
2025-04-10 12:24:23 +0200

    több termék

--- termekLista.js
+++ termekLista.js
@@ -22,6 +22,72 @@ export const termekLista = [ {
     ar: 50000,
     kep: 'dron.jfif'
 }, 
+{
+    id: 5,
+    nev: 'Apple AirPods 2',
+    ar: 114290, 
+    kep: 'airpods.png',
+}, 
+{
+    id: 6,
+    nev: 'Xiaomi Mi Watch, Amazfit',
+    ar: 39990,
+    kep: 'huawei.png'
+}, 
+{
+    id: 7,
+    nev: 'Háttérvilágítás TV-hez',
+    ar: 15000,
+    kep: 'led.png',
+}, 
+{
+    id: 8,
+    nev: 'Mini dron',
+    ar: 50000,
+    kep: 'dron.jfif'
+},
+{
+    id: 1,
+    nev: 'Apple AirPods 2',
+    ar: 114290, 
+    kep: 'airpods.png',
+}, 
+{
+    id: 2,
+    nev: 'Xiaomi Mi Watch, Amazfit',
+    ar: 39990,
+    kep: 'huawei.png'
+}, 
+{
+    id: 3,
+    nev: 'Háttérvilágítás TV-hez',
+    ar: 15000,
+    kep: 'led.png',
+}, 
+{
+    id: 4,
+    nev: 'Mini dron',
+    ar: 50000,
+    kep: 'dron.jfif'
+}, 
+{
+    id: 5,
+    nev: 'Apple AirPods 2',
+    ar: 114290, 
+    kep: 'airpods.png',
+}, 
+{
+    id: 6,
+    nev: 'Xiaomi Mi Watch, Amazfit',
+    ar: 39990,
+    kep: 'huawei.png'
+}, 
+{
+    id: 7,
+    nev: 'Háttérvilágítás TV-hez',
+    ar: 15000,
+    kep: 'led.png',
+}
 ]
 
 
