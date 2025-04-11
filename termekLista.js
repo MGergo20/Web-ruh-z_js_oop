035b12557eebbc56f729a81697a8ebfa42d31ff6
MGergo20 <medgyaszayg3@gmail.com>
2025-04-11 08:06:29 +0200

    Update termekLista.js

--- termekLista.js
+++ termekLista.js
@@ -2,92 +2,49 @@ export const termekLista = [ {
     id: 1,
     nev: 'Apple AirPods 2',
     ar: 114290, 
-    kep: 'airpods.png',
+    kep: 'kepek/airpods.png',
 }, 
 {
     id: 2,
     nev: 'Xiaomi Mi Watch, Amazfit',
     ar: 39990,
-    kep: 'huawei.png'
+    kep: 'kepek/huawei.png'
 }, 
 {
     id: 3,
     nev: 'Háttérvilágítás TV-hez',
     ar: 15000,
-    kep: 'led.png',
+    kep: 'kepek/led.png',
 }, 
 {
     id: 4,
     nev: 'Mini dron',
     ar: 50000,
-    kep: 'dron.jfif'
+    kep: 'kepek/dron.jfif'
 }, 
-{
     id: 5,
     nev: 'Apple AirPods 2',
     ar: 114290, 
-    kep: 'airpods.png',
+    kep: 'kepek/airpods.png',
 }, 
 {
     id: 6,
     nev: 'Xiaomi Mi Watch, Amazfit',
     ar: 39990,
-    kep: 'huawei.png'
+    kep: 'kepek/huawei.png'
 }, 
 {
     id: 7,
     nev: 'Háttérvilágítás TV-hez',
     ar: 15000,
-    kep: 'led.png',
+    kep: 'kepek/led.png',
 }, 
 {
     id: 8,
     nev: 'Mini dron',
     ar: 50000,
-    kep: 'dron.jfif'
-},
-{
-    id: 1,
-    nev: 'Apple AirPods 2',
-    ar: 114290, 
-    kep: 'airpods.png',
+    kep: 'kepek/dron.jfif'
 }, 
-{
-    id: 2,
-    nev: 'Xiaomi Mi Watch, Amazfit',
-    ar: 39990,
-    kep: 'huawei.png'
-}, 
-{
-    id: 3,
-    nev: 'Háttérvilágítás TV-hez',
-    ar: 15000,
-    kep: 'led.png',
-}, 
-{
-    id: 4,
-    nev: 'Mini dron',
-    ar: 50000,
-    kep: 'dron.jfif'
-}, 
-{
-    id: 5,
-    nev: 'Apple AirPods 2',
-    ar: 114290, 
-    kep: 'airpods.png',
-}, 
-{
-    id: 6,
-    nev: 'Xiaomi Mi Watch, Amazfit',
-    ar: 39990,
-    kep: 'huawei.png'
-}, 
-{
-    id: 7,
-    nev: 'Háttérvilágítás TV-hez',
-    ar: 15000,
-    kep: 'led.png',
-}
 ]
 
 
